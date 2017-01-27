f7add92ef3d01dd75d6bbc3e0dc7a9d199d8e2d9
hpneo <hpneo@hotmail.com>
2017-01-27 11:29:11 -0500

    Remove unused logs

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require 'cuba_api/cors'
 require 'cuba/send_file'
 require 'rack/protection'
 require 'erb'
-require 'logger'
 
 Cuba.use Rack::Session::Cookie, :secret => "4d4249b67c3276f5ee6fb0df413bfd9d60a72581522fbc3a26ab057343192cc12869a20b84787e8859060be5b0d49317a4d6d1a542bbf891fe6bf8667fbe0cf2"
 
@@ -18,8 +17,6 @@ Cuba.cors_setup do |cors|
   cors.expose = 'x-requested-with' # default: nil
   cors.headers = 'Access-Control-Allow-Origin=*'
 end
-
-logger = Logger.new('logs/production.log')
 
 Cuba.define do
   on_cors do
